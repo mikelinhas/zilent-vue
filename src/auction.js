24f54222cae523ed8bd790c48a8ce5df3f719a78
mzarcogillett <mzarcogillett@gmail.com>
2018-01-30 17:56:08 +0100

    added socket stuff

--- src/auction.js
+++ src/auction.js
@@ -3,9 +3,12 @@ import Vue from 'vue'
 import Navbar from './components/Navbar.vue'
 import Countdown from './components/Countdown.vue'
 import Itemlist from './auction/Itemlist.vue'
+
+import VueSocketio from 'vue-socket.io'
 import VueMasonry from 'vue-masonry-css'
 
 Vue.use(VueMasonry)
+Vue.use(VueSocketio, 'http://localhost:3000')
 
 new Vue({
 	el: '#app',
--- src/auction/Itemlist.vue
+++ src/auction/Itemlist.vue
@@ -68,6 +68,15 @@
 
         components: {Item},
 
+        sockets: {
+            connect: function(){
+                console.log("socket connected")
+            },
+            customEmit: function(val){
+                console.log("this method was fired by the socket server. eg: io.emit('customEmit', data")
+            }
+        },
+
 		methods: {
 
             queryItems: function() {
